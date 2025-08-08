58c5fd738bc9398f27f54dc5ffe98a64f3f35ee6
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-08-08 19:08:29 +0500

    users appga o'zgartirishlar kiritildi

--- users/admin.py
+++ users/admin.py
@@ -1,23 +1,48 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from unfold.admin import ModelAdmin  # unfold dan foydalanamiz
+from django.utils.html import format_html
+from .models import CustomUser, Profile
+
 
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
-    list_filter = ('role', 'is_staff', 'is_active')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+class CustomUserAdmin(ModelAdmin):
+    list_display = ('id', 'email', 'phone_number', 'role', 'is_verified', 'is_active', 'date_joined')
+    list_filter = ('role', 'is_verified', 'is_active', 'date_joined')
+    search_fields = ('email', 'phone_number')
+    ordering = ('-date_joined',)
+    list_editable = ('is_verified', 'is_active')
+
+    readonly_fields = ('last_login', 'date_joined')
 
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password', 'role')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ("Asosiy ma'lumotlar", {
+            'fields': ('email', 'phone_number', 'password', 'role', 'is_verified')
+        }),
+        ("Ruxsatlar", {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ("Tizim ma'lumotlari", {
+            'fields': ('last_login', 'date_joined')
+        }),
     )
 
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'role', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+
+@admin.register(Profile)
+class ProfileAdmin(ModelAdmin):
+    list_display = ('id', 'user', 'first_name', 'last_name', 'profile_image_preview')
+    search_fields = ('first_name', 'last_name', 'user__email', 'user__phone_number')
+
+    def profile_image_preview(self, obj):
+        if obj.profile_image:
+            return format_html('<img src="{}" width="40" style="border-radius:50%;" />', obj.profile_image.url)
+        return "-"
+    profile_image_preview.short_description = "Rasm"
+
+    fieldsets = (
+        ("Asosiy ma'lumotlar", {
+            'fields': ('user', 'first_name', 'last_name', 'date_of_birth')
+        }),
+        ("Qo‘shimcha ma'lumotlar", {
+            'fields': ('address', 'profile_image')
+        }),
     )
--- users/models.py
+++ users/models.py
@@ -10,7 +10,29 @@ class CustomUser(AbstractUser):
         DRIVER = 'driver'
         CLIENT = 'client'
 
+    username = None
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
     role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.CLIENT)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.username
+        return self.email or self.phone_number
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    profile_image = models.ImageField(upload_to='profiles/')
+    address = models.CharField(max_length=255, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}" if self.first_name else str(self.user)
